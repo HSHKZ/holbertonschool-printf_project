48a902a7619225b33001d6e377c73bdaa5087bd3
HSHKZ <lucasolegniel@gmail.com>
2024-07-23 11:12:04 +0200

    main.c main.h files

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("Hello, %s! You have %d new messages.\n", "Lucas", 5);
+        _printf("Character: %c, Percent sign: %%\n", 'M');
+        _printf("String: %s\n", NULL);
+        return (0);
+}	
